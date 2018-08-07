fa0777e01b58357332b1c534f64ad192b40674a4
Adam Sinning <asinning25@gmail.com>
2018-08-07 14:01:07 -0300

    task index links to show page created

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ get "tasks", to: "tasks#index"
 get "tasks/new", to: "tasks#new"
 post "tasks", to: "tasks#create"
 
-get "tasks/:id", to: "tasks#show"
+get "tasks/:id", to: "tasks#show", as: "show"
 
 get "tasks/:id/edit", to: "tasks#edit"
 patch  "tasks/:id", to: "tasks#update"
