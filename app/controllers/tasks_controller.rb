f59f329918b7c82b12940416c669f2e62a336b37
Adam Sinning <asinning25@gmail.com>
2018-08-07 13:07:36 -0300

    task manager updated with index to displayed completed tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,31 @@
 class TasksController < ApplicationController
+  def index
+    @tasks = Task.all
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.create(params[:task])
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def edit
+     @task = Task.find(params[:id])
+  end
+
+  def update
+    @task = Task.find(params[:id])
+    @task.update(params[:task])
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.destroy
+  end
 end
