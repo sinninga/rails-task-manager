0bc2e665397ed293d8c8d2ed2f14d4c3d3dc2e4b
Adam Sinning <asinning25@gmail.com>
2018-08-07 15:05:00 -0300

    task manager completed

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,7 +8,8 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.create(params[:task])
+    @task = Task.create(task_params)
+    redirect_to tasks_path
   end
 
   def show
@@ -21,11 +22,21 @@ class TasksController < ApplicationController
 
   def update
     @task = Task.find(params[:id])
-    @task.update(params[:task])
+    @task.update(task_params)
+
+    redirect_to task_path(@task)
   end
 
   def destroy
     @task = Task.find(params[:id])
     @task.destroy
+
+    redirect_to tasks_path
+  end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
   end
 end
